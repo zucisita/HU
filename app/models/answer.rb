de4ae51fe629dab08c4c4797a4274069b9bce29d
Azucena Reyes <zucisita@gmail.com>
2016-03-25 20:40:50 -0600

    use the concerns model, a action in severals models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,8 +1,4 @@
 class Answer < ActiveRecord::Base
-
+  include HasGravatar
   belongs_to :question
-
-  def gravatar
-    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
-  end  
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,4 @@
 class Question < ActiveRecord::Base
-
+  include HasGravatar
   has_many :answers
-
-  def gravatar
-    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
-  end
 end
