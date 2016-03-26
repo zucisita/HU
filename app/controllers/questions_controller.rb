f8eb3a079adc64b9aeb3049a47c20a0d6585ae71
Azucena Reyes <zucisita@gmail.com>
2016-03-25 19:45:16 -0600

    change file-question of folder

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,13 @@
 class QuestionsController < ApplicationController
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
   def create
     Question.create(question_params)
 
-    redirect_to root_path  
+    redirect_to root_path
   end
 
   private
