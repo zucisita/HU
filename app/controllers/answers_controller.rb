a80540fcf17ef98a3afc262dd03c79d1f97f5462
Azucena Reyes <zucisita@gmail.com>
2016-03-25 20:14:57 -0600

    update resource answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,13 @@
 class AnswersController < ApplicationController
+
+  def create
+    Answer.create(answer_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:email, :body, :question_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325233000) do
+ActiveRecord::Schema.define(version: 20160326012741) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "question_id"
+    t.string   "email"
+    t.text     "body"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "email",      null: false
