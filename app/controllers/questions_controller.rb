04777accddd711d4ba2dd169bd53230bee0ff44b
Azucena Reyes <zucisita@gmail.com>
2016-03-25 18:42:24 -0600

    show questions to the index

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+
+  def create
+    Question.create(question_params)
+
+    redirect_to root_path  
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:email, :body)
+  end
+end
