27bd86a3db3f34b56515ba9a1833d321908e6570
Azucena Reyes <zucisita@gmail.com>
2016-03-25 20:42:18 -0600

    modify the variable answers in questions_controller and view show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,7 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    @answers = @question.answers.order(created_at: :desc)
   end
 
   def create
